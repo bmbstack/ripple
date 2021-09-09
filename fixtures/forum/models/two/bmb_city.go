cece2a2c6d005411ee56a89404199616f2231a64
wangming <wangmingjob@gmail.com>
2021-09-10 00:27:19 +0800

    up, 0.5,8

--- fixtures/forum/models/two/bmb_city.go
+++ fixtures/forum/models/two/bmb_city.go
@@ -3,7 +3,6 @@ package two
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/bmbstack/ripple"
 	. "github.com/bmbstack/ripple/helper"
 	"time"
 )
@@ -13,10 +12,6 @@ type BmbCity struct {
 	Name string `json:"name" gorm:"column:name; type:varchar(50); not null; default:'北京'"`
 	Pid  int64  `json:"pid" gorm:"column:pid; type:tinyint(4); not null; default:1"`
 	BaseModel
-}
-
-func init() {
-	ripple.RegisterModels(Orm, &BmbCity{})
 }
 
 func (BmbCity) TableName() string {
@@ -36,7 +31,7 @@ func (this *BmbCity) Save() bool {
 	if this.IsDeleted == 1 {
 		this.DeletedTime = &nowTime
 	}
-	return Orm.DB.Save(this).RowsAffected == 1
+	return Orm().DB.Save(this).RowsAffected == 1
 }
 
 func (this *BmbCity) FindCityListByPid(pid int64) (list []BmbCity) {
@@ -45,7 +40,7 @@ func (this *BmbCity) FindCityListByPid(pid int64) (list []BmbCity) {
 	if IsNotEmpty(cacheValue) {
 		_ = json.Unmarshal([]byte(cacheValue), &list)
 	} else {
-		Orm.DB.Where("pid=? AND is_deleted=0", pid).Find(&list)
+		Orm().DB.Where("pid=? AND is_deleted=0", pid).Find(&list)
 		this.SetCache(cacheKey, list)
 	}
 	return list
@@ -57,7 +52,7 @@ func (this *BmbCity) FindCityID(id int64) (one BmbCity) {
 	if IsNotEmpty(cacheValue) {
 		_ = json.Unmarshal([]byte(cacheValue), &one)
 	} else {
-		Orm.DB.Where("id=? AND is_deleted=0", id).Find(&one)
+		Orm().DB.Where("id=? AND is_deleted=0", id).Find(&one)
 		this.SetCache(cacheKey, one)
 	}
 	return one
