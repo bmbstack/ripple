b415a747e869645a091e35b0a635a82e8c266fd1
wangmingjob <wangmingjob@icloud.com>
2022-02-25 15:08:51 +0800

    up, 0.7.1, yaml conf

--- ripple.go
+++ ripple.go
@@ -19,8 +19,8 @@ var firstRegModel = true
 var line1 = "=============================="
 var line2 = "================================"
 
-// VersionName 0.7.0以后使用yaml配置文件
-const VersionName = "0.7.0"
+// VersionName 0.7.1以后使用yaml配置文件
+const VersionName = "0.7.1"
 
 func Version() string {
 	return VersionName
