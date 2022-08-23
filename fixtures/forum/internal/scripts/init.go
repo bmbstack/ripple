7872b5cf1e8aa3a24f9c4d3fbc5036ed3e1a5e8d
wangmingjob <wangmingjob@icloud.com>
2022-08-23 11:04:46 +0800

    v1.2.2

--- fixtures/forum/internal/scripts/init.go
+++ fixtures/forum/internal/scripts/init.go
@@ -3,7 +3,6 @@ package scripts
 import (
 	"github.com/bmbstack/ripple"
 	"github.com/bmbstack/ripple/fixtures/forum/internal/initial"
-	"github.com/bmbstack/ripple/logger"
 	"github.com/urfave/cli/v2"
 )
 
@@ -11,9 +10,4 @@ func Init(c *cli.Context) {
 	ripple.InitConfigWithPath(c.String("env"), c.String("conf"))
 	ripple.Default().AddLogType(ripple.LogTypeSLS)
 	initial.InitLogger()
-	logger.With(map[string]interface{}{
-		"userId": 101,
-		"traceId": "lskajdfouiaadgvv",
-	}).Info("hello, tom")
-	logger.Info("hello, jack")
 }
