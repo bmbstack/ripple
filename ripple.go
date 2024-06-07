8fc510e9bcd00e18c7d284651510bae6f0ad431d
wangmingjob <wangmingjob@icloud.com>
2024-06-07 11:26:40 +0800

    update, nacos-sdk-go => v2.2.6, add, // TODO nofity

--- ripple.go
+++ ripple.go
@@ -41,7 +41,7 @@ const (
 )
 
 // VersionName 0.8.2以后使用yaml配置文件, 1.0.1升级了脚手架(protoc, ast gen)
-const VersionName = "1.2.4"
+const VersionName = "1.2.6"
 
 func Version() string {
 	return VersionName
