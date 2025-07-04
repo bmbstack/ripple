cada5f58d8e169169ff4274647d9c8fe1d8d9b3f
wangming <wangming@example.com>
2025-07-04 14:24:21 +0800

    v1.4.4

--- ripple.go
+++ ripple.go
@@ -42,7 +42,7 @@ const (
 )
 
 // VersionName 0.8.2以后使用yaml配置文件, 1.0.1升级了脚手架(protoc, ast gen)
-const VersionName = "1.4.3"
+const VersionName = "1.4.4"
 
 func Version() string {
 	return VersionName
